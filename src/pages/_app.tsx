a5df08512beeefb9bc4d8d2865b5f4b4b2abb6f2
Andrey Shirkin <107760512+hor2i@users.noreply.github.com>
2026-02-07 16:03:12 +0300

    Remove legacy src/pages directory

--- src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app'
-
-// Note: Global styles are handled by App Router's layout.tsx
-// This _app.tsx is only for Pages Router error pages (404, 500)
-
-export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
-}
